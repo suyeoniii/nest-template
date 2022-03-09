f42bf7d0a5d1f178dbb0f79e60bb461aa81eb868
suyeoniii <ssy4230@naver.com>
2022-03-09 18:58:03 +0900

    :art: add users resource
    replace user folder to users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), ConfigModule.forRoot(), UsersModule],
